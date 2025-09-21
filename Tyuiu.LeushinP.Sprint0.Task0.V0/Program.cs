2948b23d93f4aff07d621a1270b25f063630fafa
Pavel Leushin <leushinps@outlook.com>
2025-09-21 22:21:55 +0500

    Task1

--- Tyuiu.LeushinP.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.LeushinP.Sprint0.Task0.V0/Program.cs
@@ -1,2 +1,6 @@
 ﻿
-Console.WriteLine("Hello, World!"); 
+Console.WriteLine("Hello, World!");
+string name = "Паша";
+int age = 45;
+Console.WriteLine("Здравствуй меня зовут" + name + " мне " + age + " лет!");
+Console.ReadKey();
