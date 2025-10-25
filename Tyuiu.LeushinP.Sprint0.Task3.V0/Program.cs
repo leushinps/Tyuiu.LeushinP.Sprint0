da14532b6b7fc38964e80e63ae5e5c55f163616c
Pavel Leushin <leushinps@outlook.com>
2025-10-26 01:52:55 +0500

    Task3

---
+++ Tyuiu.LeushinP.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.LeushinP.Sprint0.Task3.V0.Lib;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Tyuiu.LeushinP.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadLine();
+        }
+    }
+}
